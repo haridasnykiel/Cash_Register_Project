df1c619ac456562d30e42db11469996d54b5f6d9
Haridas <haridasnykiel@gmail.com>
2018-10-16 21:45:59 +0100

    Added the initial implementation of the Cash Register project

---
+++ Cash_Register_Core/CashRegister.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Cash_Register_Core {
+    public class CashRegister {
+
+        public const decimal PENNY = 00.01M;
+        public const decimal NICKEL = 00.05M;
+        public const decimal DIME = 00.10M;
+        public const decimal QUARTER = 00.25M;
+        public const decimal HALF_DOLLAR = 00.50M;
+        public const decimal ONE = 01.00M;
+        public const decimal TWO = 02.00M;
+        public const decimal FIVE = 05.00M;
+        public const decimal TEN = 10.00M;
+        public const decimal TWENTY = 20.00M;
+        public const decimal FIFTY = 50.00M;
+        public const decimal ONE_HUNDRED = 100.00M;
+
+        private decimal _price;
+        private decimal _cash;
+
+        public CashRegister (decimal price, decimal cash) {
+            _price = price;
+            _cash = cash;
+        }
+
+        public string ERROR {
+            get {
+                return _price > _cash ? "ERROR" : null;
+            }
+        }
+
+        public decimal CashToReturn () {
+            if (_price == _cash) {
+                return 0M;
+            } else {
+                return 10.00M;
+            }
+        }
+
+    }
+}
---
+++ Cash_Register_Tests/CashRegTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Cash_Register_Core;
+using Xunit;
+
+namespace Cash_Register_Tests {
+    public class CashRegTests {
+
+        private string Error (CashRegister cashRegister) =>
+            $"The Error property != ERROR, but =  {cashRegister.ERROR}";
+
+        private string ChangeCalculationError (decimal expected, decimal actual) =>
+            $"Expected change: {expected} - Actual change: {actual}";
+
+        private string ValuesAreNotEqualError =>
+            "The input values were equal and the register did not return 0.";
+
+        [Fact]
+        public void Test1 () {
+            var cashRegister = new CashRegister (15.94M, 16.00M);
+            var change = cashRegister.CashToReturn ();
+            Assert.True (change == 0.06M, ChangeCalculationError (0.06M, change));
+        }
+
+        [Fact]
+        public void Test2 () {
+            var cashRegister = new CashRegister (17M, 16M);
+            Assert.True (cashRegister.ERROR == "ERROR", Error (cashRegister));
+        }
+
+        [Fact]
+        public void Test3 () {
+            var cashRegister = new CashRegister (35M, 35M);
+            Assert.True (cashRegister.CashToReturn () == 0, ValuesAreNotEqualError);
+        }
+    }
+}
