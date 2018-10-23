50426ee960b6fdfea7e8100f2240a75a9f10bedc
Haridas <haridasnykiel@gmail.com>
2018-10-23 22:00:46 +0100

    Completed the cash register project.

--- Cash_Register_User/Program.cs
+++ Cash_Register_User/Program.cs
@@ -4,20 +4,20 @@ namespace Cash_Register_User {
     class Program {
         static void Main (string[] args) {
             Console.WriteLine ("\nWelcome to the Cash Register Program");
-            Console.WriteLine ("\n\n====================================");
-            Console.WriteLine ("\nInput Price of item.");
+            Console.WriteLine ("\n====================================");
+            Console.WriteLine ("\n\nInput Price of item.");
             decimal price = Convert.ToDecimal (Console.ReadLine ());
             Console.WriteLine ("\nInput customer cash.");
-            Console.WriteLine ("\n====================================");
+
             decimal cash = Convert.ToDecimal (Console.ReadLine ());
 
+            Console.WriteLine ("\n\n====================================");
             var cashRegister = new CashRegister (price, cash);
             if (price > cash)
                 Console.WriteLine (cashRegister.ERROR);
             else {
-                Console.WriteLine ("Cash to return: " + cashRegister.CashToReturn ().ToString ());
-                Console.WriteLine ("\n\nChange: \n" + cashRegister.CalculateChange ());
-
+                Console.WriteLine ("Cash to return: " + cashRegister.CashToReturn ().ToString () + "\n");
+                Console.WriteLine (cashRegister.RemoveEmptyLines("Change:\n" + cashRegister.CalculateChange ()));
             }
 
         }
